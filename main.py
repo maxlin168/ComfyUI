21bdea3f31851bcee332e5bca1eb864abe3d0035
bilt <vanderbilt@gmail.com>
2025-09-06 22:24:32 +0800

    frp related

--- main.py
+++ main.py
@@ -214,7 +214,7 @@ def start_frp():
 
     # notion.add_record_to_notion_database(f"http://117.72.185.137:{port}/")
 
-    notion.add_record_to_notion_database(f"http://{uuid_str}.yesky.online")
+    notion.add_record_to_notion_database(f"https://{uuid_str}.yesky.online")
     add_backend_host_to_worker_kv(f"{uuid_str}.yesky.online")
 
     # notion.add_record_to_notion_database(f"https://k.yesky.online")
